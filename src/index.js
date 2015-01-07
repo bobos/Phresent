e6dd544145312fa20f4155faafa785f2976659dd
Veronica Xue <johnnysangel@163.com>
2015-01-07 14:59:37 +0800

    Add switch to next page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -6,10 +7,35 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/lib/img/previous.png', function(req, res) {
+    var img = fs.readFileSync('lib/img/previous.png');
+    res.writeHead(200, {'Content-ype': 'image/gif'});
+    res.end(img, 'binary');
+});
+
+
+app.get('/lib/img/next.png', function(req, res) {
+    var img = fs.readFileSync('lib/img/next.png');
+    res.writeHead(200, {'Content-ype': 'image/gif'});
+    res.end(img, 'binary');
+});
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
+
+    socket.on('load page', function(pagename) {
+        fs.readFile('slides/' + pagename.toString() + '.html', 'utf-8', function(err, data) {
+            if (err) {
+                console.log(err);
+                io.emit('load page', 'NOT EXIST');
+            }
+            else {
+                console.log(data);
+                io.emit('load page', data);
+            }
+        });
+    });
 });
 
 http.listen(3000, function(){
