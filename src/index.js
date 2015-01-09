5a6c1f3d184c5a9a83f0a5822111ba0f1e9cffef
Bo Sun <eboosun@elx14407nc-p7.(none)>
2015-01-09 13:43:29 +0800

    fix namespace broadcast problem

--- src/index.js
+++ src/index.js
@@ -15,10 +15,6 @@ app.get('/audienceChannel', function(req, res){
   res.sendFile(__dirname + '/audience.html');
 });
 
-app.get('/question', function(req, res) {
-    res.sendFile(__dirname + '/question.html');
-});
-
 // images, js, css etc.  
 app.get('/lib/*', function(req, res){
   res.sendFile(__dirname + '/lib/' + req.params[0]);
@@ -26,10 +22,10 @@ app.get('/lib/*', function(req, res){
 
 var askSlide = 'load slide'
 function load_slide(pageNum, isPresenter, socket) {
-  fs.readFile('slides/' + pageNum + '.html', 'utf-8', function(err, data) {
+  fs.readFile('slides/' + pageNum.toString() + '.html', 'utf-8', function(err, data) {
       var msg = data;
       if (err) { msg = 'THE END'; }
-      if (!err && isPresenter) { pageNumber = parseInt(pageNum); }
+      if (!err && isPresenter) { pageNumber = pageNum; }
       socket.emit(askSlide, msg);
   });
 }
@@ -41,13 +37,14 @@ var presenterChannel = io.of('/presenterChannel');
 presenterChannel.on('connection', function(socket){
   // load slide request from presenter
   socket.on(askSlide, function(incr) {
-    load_slide(pageNumber + parseInt(incr), true, socket);
+    load_slide(pageNumber + incr, true, socket);
     });
 
   socket.on('ask question', function(question) {
     questions.push(question);
     console.log(question);
-    socket.emit('change question number', questions.length);
+    presenterChannel.emit('change question number', 
+                          questions.length);
     });
 });
 
@@ -58,7 +55,7 @@ var audienceChannel = io.of('/audienceChannel');
 audienceChannel.on('connection', function(socket){
 // load slide request from audience
   socket.on(askSlide, function(num) {
-    load_slide(parseInt(num), false, socket)
+    load_slide(num, false, socket)
     });
 });
 
